22d0bba94d7caaa12144061aeb67de2e437a0b90
Tagca <127929016+CandyRGB@users.noreply.github.com>
2025-05-06 20:21:49 +0800

    Update index.vue
    组合式模板

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,42 +7,32 @@
   </view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
+<script setup>
+  const title = "Hello"
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
+  .text-area {
+    display: flex;
+    justify-content: center;
+    .title {
+      font-size: 36rpx;
+      color: #8f8f94;
+    }
+  }
 }
 </style>
