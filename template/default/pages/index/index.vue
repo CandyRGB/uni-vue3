f57288a386c373e183946452badea2160290c4f9
Tagca <127929016+CandyRGB@users.noreply.github.com>
2025-05-06 20:04:35 +0800

    Update index.vue

--- template/default/pages/index/index.vue
+++ template/default/pages/index/index.vue
@@ -7,43 +7,24 @@
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	const title = "Hello"
 </script>
 
-<style>
+<style lang="scss" scoped>
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		.logo {
+			height: 200rpx;
+			width: 200rpx;
+			margin: 200rpx auto 50rpx auto;
+		}
+		.title {
+			font-size: 36rpx;
+			color: #8f8f94;
+		}
 	}
 </style>
